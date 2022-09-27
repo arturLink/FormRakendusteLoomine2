0b59e99d291c179b3295b45754702bca7efc9f1b
opilane <opilane@HP-CZC6297B8L>
2022-09-27 15:04:29 +0300

    Add project files.

---
+++ FormRakendusteLoomine2/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormRakendusteLoomine2
+{
+    public partial class MainForm : Form
+    {
+        Button Pildid;
+        Button MatMang;
+        Button PiltMang;
+        public MainForm()
+        {
+            Height = 720;
+            Width = 1280;
+            BackColor = Color.LightBlue;
+            ForeColor = Color.Black;
+            Text = "Peamine Vorm";
+
+            Pildid = new Button();
+            Pildid.Text = "Pildi vaatamise";
+            Pildid.ForeColor = Color.Black;
+            Pildid.BackColor = Color.White;
+            Pildid.Width = 150;
+            Pildid.Height = 50;
+            Pildid.Location = new Point(150, 300);
+            this.Controls.Add(Pildid);
+            Pildid.Click += Pildid_Click;
+
+            MatMang = new Button();
+            MatMang.Text = "Matemaatiline äraarvamismäng";
+            MatMang.ForeColor = Color.Black;
+            MatMang.BackColor = Color.White;
+            MatMang.Width = 150;
+            MatMang.Height = 50;
+            MatMang.Location = new Point(550, 300);
+            this.Controls.Add(MatMang);
+
+            PiltMang = new Button();
+            PiltMang.Text = "Piltide leidmise mäng";
+            PiltMang.ForeColor = Color.Black;
+            PiltMang.BackColor = Color.White;
+            PiltMang.Width = 150;
+            PiltMang.Height = 50;
+            PiltMang.Location = new Point(950, 300);
+            this.Controls.Add(PiltMang);
+        }
+
+        private void Pildid_Click(object sender, EventArgs e)
+        {
+            PiltVaatamine piltVaatamine = new PiltVaatamine();
+            piltVaatamine.ShowDialog();
+        }
+    }
+}
---
+++ FormRakendusteLoomine2/PiltVaatamine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormRakendusteLoomine2
+{
+    public class PiltVaatamine : Form
+    {
+
+
+        PictureBox pilt;
+        TableLayoutPanel tableLayoutPanel1;
+        public PiltVaatamine()
+        {
+            this.ClientSize = new System.Drawing.Size(720, 480);
+            this.Text = "Pildid";
+
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.Controls.Add(this.tableLayoutPanel1);
+
+            pilt = new PictureBox()
+            {
+                Image = new Bitmap("snow.jpg"),
+                BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                Location = new System.Drawing.Point(2, 2),
+                Size = new System.Drawing.Size(100,100),
+                TabIndex = 0,
+                TabStop = false
+            };
+            tableLayoutPanel1.Controls.Add(pilt,0,0);
+            tableLayoutPanel1.SetCellPosition(pilt, new TableLayoutPanelCellPosition(0, 0));
+            tableLayoutPanel1.SetColumnSpan(pilt, 2);
+
+            
+        }
+    }
+}
